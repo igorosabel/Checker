68c55f7e44b2f7460a7d23532c129eea9b9f78c3
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2024-04-04 00:39:08 +0200

    Avanzo con modal Checkin

--- src/app/modules/shared/components/checkin/checkin.component.ts
+++ src/app/modules/shared/components/checkin/checkin.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import {
+  Component,
+  OutputEmitterRef,
+  WritableSignal,
+  inject,
+  output,
+  signal,
+} from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardTitle,
+} from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { CheckinType } from '@app/model/checkintype.model';
+import { UserService } from '@app/services/user.service';
+import { Checkin } from '@model/checkin.model';
 
 @Component({
   selector: 'app-checkin',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatIconButton,
+    MatIcon,
+    MatSelect,
+    MatOption,
+  ],
   templateUrl: './checkin.component.html',
   styleUrl: './checkin.component.scss',
 })
-export default class CheckinComponent {}
+export default class CheckinComponent {
+  us: UserService = inject(UserService);
+
+  checkinTypeList: CheckinType[] = this.us.checkinTypeList;
+
+  showModal: WritableSignal<boolean> = signal<boolean>(false);
+  modalTitle: string = '';
+  selectedCheckin: Checkin = new Checkin();
+  //nameBox: Signal<ElementRef> = viewChild.required<ElementRef>('nameBox');
+
+  saved: OutputEmitterRef<void> = output();
+
+  load(c: Checkin): void {
+    this.selectedCheckin = c;
+    if (c.id === null) {
+      this.modalTitle = 'Nuevo Checkin';
+    } else {
+      this.modalTitle = 'Checkin';
+    }
+    this.showModal.set(true);
+    //window.setTimeout((): void => {
+    //this.nameBox().nativeElement.focus();
+    //}, 100);
+  }
+
+  closeModal(): void {
+    this.showModal.set(false);
+  }
+}
--- src/app/modules/shared/components/footer/footer.component.ts
+++ src/app/modules/shared/components/footer/footer.component.ts
@@ -1,9 +1,17 @@
 import { NgClass } from '@angular/common';
-import { Component, InputSignal, input } from '@angular/core';
+import {
+  Component,
+  InputSignal,
+  Signal,
+  input,
+  viewChild,
+} from '@angular/core';
 import { MatFabButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { Checkin } from '@app/model/checkin.model';
+import CheckinComponent from '@shared/components/checkin/checkin.component';
 
 @Component({
   selector: 'app-footer',
@@ -15,10 +23,16 @@ import { RouterLink } from '@angular/router';
     MatIcon,
     RouterLink,
     NgClass,
+    CheckinComponent,
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export default class FooterComponent {
   selected: InputSignal<string> = input.required<string>();
+  c: Signal<CheckinComponent> = viewChild.required<CheckinComponent>('c');
+
+  addCheckin(): void {
+    this.c().load(new Checkin());
+  }
 }
