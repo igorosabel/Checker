51804893bfab85c7e001ead7e00713fe2ced9e64
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2024-04-03 08:51:01 +0200

    Corrección en login, estilos para footer

--- src/app/modules/shared/components/footer/footer.component.ts
+++ src/app/modules/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, InputSignal, input } from '@angular/core';
 import { MatFabButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
@@ -7,8 +8,17 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatToolbar, MatIconButton, MatFabButton, MatIcon, RouterLink],
+  imports: [
+    MatToolbar,
+    MatIconButton,
+    MatFabButton,
+    MatIcon,
+    RouterLink,
+    NgClass,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export default class FooterComponent {}
+export default class FooterComponent {
+  selected: InputSignal<string> = input.required<string>();
+}
