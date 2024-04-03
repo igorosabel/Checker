2f8d8ef0744d8dfb3be452eefb0381b692d0391a
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2024-04-03 13:14:01 +0200

    Checkin types terminado

--- src/app/modules/pages/checkin-types/checkin-types.component.ts
+++ src/app/modules/pages/checkin-types/checkin-types.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { MatFabButton } from '@angular/material/button';
-import { MatCard, MatCardContent } from '@angular/material/card';
+import { NgClass } from '@angular/common';
+import {
+  Component,
+  OnInit,
+  WritableSignal,
+  inject,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  MatButton,
+  MatFabButton,
+  MatIconButton,
+} from '@angular/material/button';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardTitle,
+} from '@angular/material/card';
+import { MatFormField, MatHint, MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 import {
   MatList,
   MatListItem,
   MatListItemLine,
   MatListItemTitle,
 } from '@angular/material/list';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { CheckinTypesResult } from '@interfaces/checkins.interfaces';
+import { StatusResult } from '@interfaces/interfaces';
 import { CheckinType } from '@model/checkintype.model';
 import { ApiService } from '@services/api.service';
 import { ClassMapperService } from '@services/class-mapper.service';
+import { DialogService } from '@services/dialog.service';
 import HeaderComponent from '@shared/components/header/header.component';
 
 @Component({
@@ -20,13 +43,25 @@ import HeaderComponent from '@shared/components/header/header.component';
   imports: [
     HeaderComponent,
     MatCard,
+    MatCardHeader,
+    MatCardTitle,
     MatCardContent,
+    MatCardActions,
     MatList,
     MatListItem,
     MatListItemTitle,
     MatListItemLine,
     MatFabButton,
+    MatIconButton,
     MatIcon,
+    NgClass,
+    MatButton,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatHint,
+    MatSlideToggle,
+    FormsModule,
   ],
   templateUrl: './checkin-types.component.html',
   styleUrl: './checkin-types.component.scss',
@@ -34,12 +69,109 @@ import HeaderComponent from '@shared/components/header/header.component';
 export default class CheckinTypesComponent implements OnInit {
   as: ApiService = inject(ApiService);
   cms: ClassMapperService = inject(ClassMapperService);
+  ds: DialogService = inject(DialogService);
 
   checkinTypes: CheckinType[] = [];
+  showModal: WritableSignal<boolean> = signal<boolean>(false);
+  modalTitle: string = '';
+  selectedCheckinType: CheckinType = new CheckinType();
+  checkinTypeValidName: boolean = true;
 
   ngOnInit(): void {
+    this.loadCheckinTypes();
+  }
+
+  loadCheckinTypes(): void {
     this.as.getCheckinTypes().subscribe((result: CheckinTypesResult): void => {
       this.checkinTypes = this.cms.getCheckinTypes(result.list);
     });
+  }
+
+  addNew(): void {
+    this.selectedCheckinType = new CheckinType();
+    this.modalTitle = 'Nuevo tipo de Checkin';
+    this.showModal.set(true);
+  }
+
+  editCheckinType(ct: CheckinType): void {
+    this.selectedCheckinType = ct;
+    this.modalTitle = 'Editar tipo de Checkin';
+    this.showModal.set(true);
+  }
+
+  deleteCheckinType(ct: CheckinType): void {
+    this.ds
+      .confirm({
+        title: 'Borrar tipo de Checkin',
+        content: `¿Estás seguro de querer borrar el tipo de Checkin "${ct.name}"?`,
+        ok: 'Continuar',
+        cancel: 'Cancelar',
+      })
+      .subscribe((conf: boolean): void => {
+        if (conf && ct.id !== null) {
+          this.confirmDeleteCheckin(ct.id);
+        }
+      });
+  }
+
+  confirmDeleteCheckin(id: number): void {
+    this.as.deleteCheckinType(id).subscribe((result: StatusResult): void => {
+      if (result.status === 'ok') {
+        this.ds
+          .alert({
+            title: 'Tipo de Checkin borrado',
+            content:
+              'El tipo de Checkin y todos sus Checkins asociados han sido borrados.',
+            ok: 'Continuar',
+          })
+          .subscribe((): void => {
+            this.loadCheckinTypes();
+          });
+      } else {
+        this.ds.alert({
+          title: 'Error',
+          content: 'Ocurrió un error al borrar el tipo de Checkin.',
+          ok: 'Continuar',
+        });
+      }
+    });
+  }
+
+  closeModal(): void {
+    this.showModal.set(false);
+  }
+
+  checkForm(): void {
+    this.checkinTypeValidName = true;
+    if (
+      this.selectedCheckinType.name === null ||
+      this.selectedCheckinType.name === ''
+    ) {
+      this.checkinTypeValidName = false;
+      return;
+    }
+
+    this.as
+      .saveCheckinType(this.selectedCheckinType.toInterface())
+      .subscribe((result: StatusResult): void => {
+        if (result.status === 'ok') {
+          this.ds
+            .alert({
+              title: 'Datos guardados',
+              content: 'Los datos del tipo de Checkin han sido guardados.',
+              ok: 'Continuar',
+            })
+            .subscribe((): void => {
+              this.loadCheckinTypes();
+              this.closeModal();
+            });
+        } else {
+          this.ds.alert({
+            title: 'Error',
+            content: 'Ocurrió un error al guardar el tipo de Checkin.',
+            ok: 'Continuar',
+          });
+        }
+      });
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { StatusResult } from '@app/interfaces/interfaces';
 import { environment } from '@env/environment';
-import { CheckinTypesResult } from '@interfaces/checkins.interfaces';
+import {
+  CheckinTypeInterface,
+  CheckinTypesResult,
+} from '@interfaces/checkins.interfaces';
 import {
   LoginData,
   LoginResult,
@@ -27,6 +31,20 @@ export class ApiService {
     return this.http.post<CheckinTypesResult>(
       environment.apiUrl + '/get-checkin-types',
       {}
+    );
+  }
+
+  saveCheckinType(ct: CheckinTypeInterface): Observable<StatusResult> {
+    return this.http.post<StatusResult>(
+      environment.apiUrl + '/save-checkin-type',
+      ct
+    );
+  }
+
+  deleteCheckinType(id: number): Observable<StatusResult> {
+    return this.http.post<StatusResult>(
+      environment.apiUrl + '/delete-checkin-type',
+      { id }
     );
   }
 
